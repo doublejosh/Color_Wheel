44e4229a07cea00a3c3c0ebafd96af4da2aacb4e
Josh Lind <joshuaklind@gmail.com>
2022-11-01 12:17:05 -0700

    working game

--- Color_Wheel.ino
+++ Color_Wheel.ino
@@ -1,37 +1,100 @@
 const unsigned int PIN_LED_RED = 5;
 const unsigned int PIN_LED_GREEN = 6;
 const unsigned int PIN_LED_BLUE = 9;
+const unsigned int PIN_BUTTON_1 = 2;
+const unsigned int PIN_BUTTON_2 = 3;
+const unsigned int PIN_BUTTON_3;
+const unsigned int PIN_BUTTON_4;
+const unsigned int PIN_BUTTON_5;
+const unsigned int PIN_BUTTON_6;
 
 const int ANALOG_PIN = A0;
 
-const unsigned int BLINK_DELAY = 250;
-const unsigned int BLINK_NUMBER = 7;
-const unsigned int ROTATION_DELAY = 5;
-const unsigned int GUESS_DELAY = 10000;
+const unsigned int ROTATION_DELAY = 1;
+const unsigned int GUESS_LOOP_MAX = 10000;
+const unsigned int DARK_PAUSE_DELAY = 250;
+const unsigned int FAIL_DELAY = 3000;
+const unsigned int WIN_DELAY = 3000;
+const unsigned int BLINK_DELAY = 100;
+const unsigned int BLINK_NUMBER = 20;
 
 unsigned int color[3];
+boolean gameOver = true;
+boolean correct = false;
+unsigned int readCount;
+unsigned int randomPrev;
 unsigned int randomColor;
 
+unsigned int button1;
+unsigned int button2;
+unsigned int button3;
+unsigned int button4;
+unsigned int button5;
+unsigned int button6;
+
 void setup() {
+  Serial.begin(9600);
   pinMode(PIN_LED_RED, OUTPUT);
   pinMode(PIN_LED_GREEN, OUTPUT);
   pinMode(PIN_LED_BLUE, OUTPUT);
+  pinMode(PIN_BUTTON_1, INPUT);
   randomSeed(analogRead(ANALOG_PIN));
 }
 
 void loop() {
-  rotateColors(3);
-  setColor(0, 0, 0);
-  delay(1000);
-  pickRandomColor();
+  if (gameOver) {
+    // reset round
+    gameOver = false;
+    readCount = 0;
+    rotateColors(3);
+    setColor(0, 0, 0);
+    delay(DARK_PAUSE_DELAY);
+    pickRandomColor();
+  } else if (readCount < GUESS_LOOP_MAX) {
+    // accept input
+    button1 = digitalRead(PIN_BUTTON_1);
+    button2 = digitalRead(PIN_BUTTON_2);
+//    button3 = digitalRead(PIN_BUTTON_3);
+//    button4 = digitalRead(PIN_BUTTON_4);
+//    button5 = digitalRead(PIN_BUTTON_5);
+//    button6 = digitalRead(PIN_BUTTON_6);
+    // detemine match
+    if (button1 == HIGH ||
+        button2 == HIGH ||
+        button3 == HIGH || 
+        button4 == HIGH || 
+        button5 == HIGH || 
+        button6 == HIGH) {
 
-  // accept input
-
-  // detemine match
-
-  // signal success
+      if ((randomColor == 0 && button1 == HIGH) || 
+          (randomColor == 1 && button2 == HIGH) ||
+          (randomColor == 2 && button3 == HIGH) || 
+          (randomColor == 3 && button4 == HIGH) ||
+          (randomColor == 4 && button5 == HIGH) ||
+          (randomColor == 5 && button6 == HIGH)) {
+        // CORRECT!
+        Serial.println("CORRECT");
+        blinkColor(255, 255, 255);
+        setColor(0, 0, 0);
+        delay(WIN_DELAY);
+        gameOver = true; 
+      } else {
+        // WRONG!
+        Serial.println("WRONG");
+        setColor(0, 0, 0);
+        delay(FAIL_DELAY);
+        gameOver = true;
+      }
+    }
+    readCount++;
+    delay(1);
+  } else {
+    // end of listening loop
+    gameOver = true;
+    readCount = 0;
+    correct = false;
+  }
 }
-
 
 void rotateColors (unsigned int count) {
   for (int i = 0; i < count; i += 1) {
@@ -53,29 +116,42 @@ void rotateColors (unsigned int count) {
 }
 
 void pickRandomColor () {
-  randomColor = random(5);
-  switch (randomColor) {
+  while (randomPrev == randomColor) {
+    randomColor = random(5);
+  }
+  randomPrev = randomColor;
+  Serial.println(randomColor);
+  setColorInt(randomColor);
+}
+
+void setColorInt (unsigned int color) {
+  switch (color) {
     case 0:
       setColor(255, 0, 0); // Red
+      break;
     case 1:
       setColor(255, 80, 0); // Orange
+      break;
     case 2:
       setColor(255, 255, 0); // Yellow
+      break;
     case 3:
       setColor(0, 255, 1); // Green
+      break;
     case 4:
       setColor(0, 0, 255); // Blue
+      break;
     case 5:
       setColor(255, 0, 255); // Magenta
+      break;
   }
-  delay(GUESS_DELAY);
 }
 
 void blinkColor (unsigned int red, unsigned int green, unsigned int blue) {
   for (int i = 0; i < BLINK_NUMBER; i += 1) {
     setColor(0, 0, 0);
     delay(BLINK_DELAY);
-    setColor(red, green, blue);
+    setColorInt(randomColor);
     delay(BLINK_DELAY);
   }
 }
